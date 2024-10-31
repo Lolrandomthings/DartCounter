09a73485a149b6a8a4e2c1ca8668cf8d029220c1
Laura Sanchez <laurafabiola.s.a@gmail.com>
2024-10-31 12:51:41 +0100

    databases oppdateres live

--- testdata.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO darttabell (navn, kast1, kast2) VALUES ('Laura', 30, 45);
-INSERT INTO darttabell (navn, kast1, kast2) VALUES ('Mark', 25, 50);
-INSERT INTO darttabell (navn, kast1, kast2) VALUES ('Emma', 45, 30);
-INSERT INTO darttabell (navn, kast1, kast2) VALUES ('Sophie', 20, 55);
-INSERT INTO darttabell (navn, kast1, kast2) VALUES ('Alex', 30, 30);
-INSERT INTO darttabell (navn, kast1, kast2) VALUES ('Chris', 50, 40);
-
-SELECT * FROM darttabell ORDER BY date_played DESC;
-
